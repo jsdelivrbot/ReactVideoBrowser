8e71f5127766125ca7864a020077b22041ed488c
Hudolf Hess <hudolf@gmail.com>
2017-03-01 13:45:27 -0300

    Handle on change event on SearchBar

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -1,8 +1,12 @@
 import React from 'react';
 
 class SearchBar extends React.Component {
+  onInputChange(event) {
+    console.log(event.target.value);
+  }
+
   render() {
-    return <input />
+    return <input onChange={this.onInputChange} />;
   }
 }
 
