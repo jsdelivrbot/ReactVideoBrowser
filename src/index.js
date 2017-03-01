56208e5fa703f9c2dfd75052b8735f13de886a94
Hudolf Hess <hudolf@gmail.com>
2017-03-01 13:29:27 -0300

    SearchBar component (example)

--- src/index.js
+++ src/index.js
@@ -1,8 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import SearchBar from './components/search_bar'
+
+const YOUTUBE_API_KEY = 'AIzaSyB6XRA73s2pTMgaH8Rn4AxeEXvGjkzl5Tw';
 
 const App = () => {
-  return <div>Component here...</div>;
-}
+  return (
+    <div>
+      <SearchBar />
+    </div>
+  );
+};
 
 ReactDOM.render(<App />, document.querySelector('.container'));
